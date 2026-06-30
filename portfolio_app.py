4dfd608b5166be3235046af74969b672d37f8f17
nandhinikarunagaran63-sv <nandhinikarunagaran63@gmail.com>
2026-06-30 11:20:16 +0530

    Update link for Live AI Resume Dashboard

--- portfolio_app.py
+++ portfolio_app.py
@@ -159,7 +159,7 @@ else:
                 # High-visibility action button that links directly to your deployed dashboard
                 st.link_button(
                     label="🔗 Launch Live AI Resume Dashboard", 
-                    url="https://streamlit.app", # Replace with your real deployed app link later!
+                    url="https://github.com/nandhinikarunagaran63-sv/data-portfolio/blob/main/dashboard_app.py", # Replace with your real deployed app link later!
                     type="primary"
                 )
                 st.markdown("---")
